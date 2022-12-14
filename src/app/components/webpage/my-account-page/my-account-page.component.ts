546adff55cd961d497696a60ec6a56e7aa8c8d6a
Liam Nelski <ljnelski@gmail.com>
2022-12-13 21:39:22 -0500

    Implemented buttons in my Account fopage for Service Provider

--- src/app/components/webpage/my-account-page/my-account-page.component.ts
+++ src/app/components/webpage/my-account-page/my-account-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { UserRole } from 'src/app/models/user_roles';
+import { AuthService } from 'src/app/service/auth-service/auth.service';
 
 @Component({
   selector: 'app-my-account-page',
   templateUrl: './my-account-page.component.html',
-  styleUrls: ['./my-account-page.component.css']
+  styleUrls: ['./my-account-page.component.css'],
 })
 export class MyAccountPageComponent {
+  userRole = UserRole;
 
+  constructor(public authService: AuthService) {}
 }
--- src/app/service/auth-service/auth.service.ts
+++ src/app/service/auth-service/auth.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { User } from 'src/app/models/user';
+import { UserRole } from 'src/app/models/user_roles';
 import { ApiService } from '../api-service/api.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
   private _isLoggedIn$ = new BehaviorSubject<boolean>(false);
-  private readonly TOKEN_NAME = "user_auth"
+  private readonly TOKEN_NAME = 'user_auth';
 
   isLoggedIn$ = this._isLoggedIn$.asObservable();
   user!: User;
 
   get currentUser() {
-    return JSON.parse(localStorage.getItem(this.TOKEN_NAME))
+    return JSON.parse(localStorage.getItem(this.TOKEN_NAME));
   }
 
   constructor(private apiService: ApiService) {
@@ -25,18 +25,30 @@ export class AuthService {
 
   login(username: string, password: string) {
     return this.apiService.loginUser(username, password).pipe(
-      tap(response => {
-        console.log("login response: ", response)
+      tap((response) => {
+        console.log('login response: ', response);
         this._isLoggedIn$.next(true);
         localStorage.setItem(this.TOKEN_NAME, JSON.stringify(response));
-        this.user = response
+        this.user = response;
       })
-    )
+    );
   }
 
   logout() {
     this._isLoggedIn$.next(false);
-    localStorage.removeItem(this.TOKEN_NAME)
-    this.user = null
+    localStorage.removeItem(this.TOKEN_NAME);
+    this.user = null;
+  }
+
+  hasRole(requiredRole: UserRole): boolean {
+    if (!this.user) {
+      return false;
+    } else {
+      return this.user.roles.includes(requiredRole);
+    }
+  }
+
+  lacksRole(requiredRole: UserRole) : boolean {
+    return !this.hasRole(requiredRole)
   }
 }
