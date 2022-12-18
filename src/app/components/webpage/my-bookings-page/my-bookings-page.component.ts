a571f614c3ea5b25bb2c6dfd72ee11939c6d3811
Yahya Ibrahim <yahyaibrahim96@gmail.com>
2022-12-18 01:57:32 -0500

    Added more content for booking-page

--- src/app/components/webpage/my-bookings-page/my-bookings-page.component.ts
+++ src/app/components/webpage/my-bookings-page/my-bookings-page.component.ts
@@ -7,21 +7,17 @@ import {MatTable} from '@angular/material/table';
 export interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
+  date: string;
   symbol: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Liam', date: 'Jan 5, 2023', symbol: '8am'},
+  {position: 2, name: 'Manav', date: 'Jan 10, 2023', symbol:'10am'},
+  {position: 3, name: 'Jose', date: 'Jan 13, 2023', symbol: '12pm'},
+  {position: 4, name: 'Shay', date: 'Jan 16, 2023', symbol: '2pm'},
+  {position: 5, name: 'Abhishake', date: 'Jan 20, 2023', symbol: '6pm'},
+
 ];
 
 
@@ -33,7 +29,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class MyBookingsPageComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'date', 'symbol'];
   dataSource = [...ELEMENT_DATA];
 
   @ViewChild(MatTable) table: MatTable<PeriodicElement>;
